6ace405fa7f5838b5248b09328ffc4663311dfa4
Yashwant Choudhary <84570757+Yashwant077@users.noreply.github.com>
2023-11-19 10:41:43 +0530

    Create 19-11-23-Intersection_of_two_sorted_Linked_lists.cpp

---
+++ 19-11-23-Intersection_of_two_sorted_Linked_lists.cpp
@@ -0,0 +1,147 @@
+/*
+Problem Description: Intersection of two sorted Linked lists
+
+Given two linked lists sorted in increasing order, create a new linked list representing the intersection of the two linked lists. The new linked list should be made with without changing the original lists.
+
+Note: The elements of the linked list are not necessarily distinct.
+
+Example 1:
+
+Input:
+LinkedList1 = 1->2->3->4->6
+LinkedList2 = 2->4->6->8
+Output: 2 4 6
+Explanation: For the given two
+linked list, 2, 4 and 6 are the elements
+in the intersection.
+
+Example 2:
+
+Input:
+LinkedList1 = 10->20->40->50
+LinkedList2 = 15->40
+Output: 40
+Your Task:
+You don't have to take any input of print anything. Your task is to complete the function findIntersection(), which will take head of both of the linked lists as input and should find the intersection of two linked list and add all the elements in intersection to the third linked list and return the head of the third linked list.
+
+Expected Time Complexity : O(n+m)
+Expected Auxilliary Space : O(n+m)
+Note: n, m are the size of the respective linked lists.
+
+Constraints:
+1 <= size of linked lists <= 5000
+1 <= Data in linked list nodes <= 10^4
+
+*/
+
+
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    if(n)
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    else cout<< " ";
+}
+
+
+// } Driver Code Ends
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        // code goes here.
+        Node* dummy=new Node(0);
+        Node* p1=head1, *p2=head2, *p=dummy;
+        while(p1 && p2){
+            if(p1->data == p2->data){
+                p->next=new Node(p1->data);
+                p=p->next;
+                p1=p1->next;
+                p2=p2->next;
+            }
+            else if(p1->data < p2->data) p1=p1->next;
+            else p2=p2->next;
+        }
+        return dummy->next;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Solution ob;
+	    Node* result = ob.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
